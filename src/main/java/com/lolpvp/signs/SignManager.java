61e18b6988ee5a31136e18213f7eaba82f2c982f
Spaces <spaceslulz@hotmail.com>
2017-06-06 00:18:56 -1000

    Intergrated Vault into charging for executing a sign if it has a price.

--- src/main/java/com/lolpvp/signs/SignManager.java
+++ src/main/java/com/lolpvp/signs/SignManager.java
@@ -1,29 +1,52 @@
 package com.lolpvp.signs;
 
 import com.lolpvp.core.Core;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.ChatColor;
 import org.bukkit.block.Sign;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.logging.Level;
 
 public class SignManager {
 
     private FileConfiguration signsData;
+    private File signsFile;
     private Core plugin;
 
     public SignManager(Core instance) {
         this.plugin = instance;
+        signsFile = new File(instance.getDataFolder() + "signs.yml");
+        if(!signsFile.exists()) {
+            try {
+                signsFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                instance.getLogger().log(Level.WARNING,"Couldn't create signs.yml");
+            }
+        }
+        signsData = YamlConfiguration.loadConfiguration(signsFile);
     }
 
     public void createCommandSign(Sign sign, List<String> commands) {
+        createCommandSign(sign, commands, 0);
     }
 
     public void createCommandSign(Sign sign, List<String> commands, int price) {
-        sign.setMetadata("commandsign-id", new FixedMetadataValue(this.plugin, UUID.randomUUID().toString()));
-        if(price > 0) sign.setMetadata("commandsign-price", new FixedMetadataValue(this.plugin, price));
+        String id = UUID.randomUUID().toString()
+        sign.setMetadata("commandsign-id", new FixedMetadataValue(this.plugin, id));
+        if(price > 0) {
+            sign.setMetadata("commandsign-price", new FixedMetadataValue(this.plugin, price));
+            signsData.set(id + ".price", price);
+        }
         StringBuilder commandsString = new StringBuilder();
         commands.forEach(command -> {
             if(command == commands.get(commands.size() - 1)) {
@@ -33,13 +56,8 @@ public class SignManager {
             }
         });
         sign.setMetadata("commandsign-commands", new FixedMetadataValue(this.plugin, commandsString.toString()));
-    }
+        signsData.set(id + ".commands", commands);
 
-    public void executeSignCommands(Sign sign) {
-        String[] commands = sign.getMetadata("commandsign-price").get(0).asString().split("|");
-        Arrays.stream(commands).forEach(command -> {
-            this.plugin.getServer().dispatchCommand(this.plugin.getServer().getConsoleSender(), command);
-        });
     }
 
     public void removeCommandSign(Sign sign) {
@@ -48,6 +66,20 @@ public class SignManager {
         sign.removeMetadata("commandsign-id", this.plugin);
         sign.removeMetadata("commandsign-commands", this.plugin);
         sign.getBlock().breakNaturally();
+    }
+
+    public void executeSignCommands(Sign sign, Player player) {
+        if(sign.hasMetadata("commandsign-price")) {
+            EconomyResponse response = Core.getEconomy().withdrawPlayer(player, sign.getMetadata("commandsign-price").get(0).asInt());
+            if(!response.transactionSuccess()) {
+                player.sendMessage(ChatColor.RED + "Insufficient funds.");
+                return;
+            }
+        }
+        String[] commands = sign.getMetadata("commandsign-commands").get(0).asString().split("|");
+        Arrays.stream(commands).forEach(command -> {
+            this.plugin.getServer().dispatchCommand(this.plugin.getServer().getConsoleSender(), command);
+        });
     }
 
     public boolean isCommandSign(Sign sign) {
